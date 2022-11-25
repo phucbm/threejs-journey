991f3295c4af02346be2782bf62076f31da7c25d
phucbm <hiphucbui@gmail.com>
2022-11-25 16:19:20 +0700

    move from webpack to nuxt

---
+++ pages/00.vue
@@ -0,0 +1,14 @@
+<script setup>
+import * as THREE from 'three';
+
+const canvasRef = ref(null);
+
+onMounted(() => {
+  const canvas = canvasRef.value;
+});
+</script>
+
+
+<template>
+  <canvas ref="canvasRef"></canvas>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+
+<template>
+  <h1>Threejs Journey</h1>
+</template>
+
+<style scoped>
+
+</style>
