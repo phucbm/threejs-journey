73dc22a1ddf145b46fb0e3d055cbfda02466d792
phucbui_viivue <phuc.bui@viivue.com>
2023-02-07 21:36:20 +0700

    feat(20-scroll-based-animation): create objects with parallax

---
+++ pages/17.vue
@@ -0,0 +1,240 @@
+<script setup>
+import * as THREE from 'three';
+import * as dat from "lil-gui";
+
+definePageMeta({title: `Scroll based animation`});
+
+const route = useRoute();
+useHead({
+  title: route.meta.title
+});
+
+const canvasRef = ref(null);
+
+onMounted(() => {
+  const canvas = canvasRef.value;
+
+  /**
+   * Debug
+   */
+  const gui = new dat.GUI()
+
+  const parameters = {
+    materialColor: '#ffeded'
+  }
+
+  gui
+      .addColor(parameters, 'materialColor')
+      .onChange(() => {
+        material.color.set(parameters.materialColor)
+      })
+
+  /**
+   * Base
+   */
+      // Scene
+  const scene = new THREE.Scene()
+
+  /**
+   * Objects
+   */
+      // Texture
+  const textureLoader = new THREE.TextureLoader()
+  const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
+  gradientTexture.magFilter = THREE.NearestFilter
+
+  // Material
+  const material = new THREE.MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+  })
+
+  // Meshes
+  const objectDistance = 4
+  const mesh1 = new THREE.Mesh(
+      new THREE.TorusGeometry(1, 0.4, 16, 60),
+      material
+  );
+  const mesh2 = new THREE.Mesh(
+      new THREE.ConeGeometry(1, 2, 32),
+      material
+  );
+  const mesh3 = new THREE.Mesh(
+      new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+      material
+  );
+
+  mesh1.position.y = -objectDistance * 0
+  mesh2.position.y = -objectDistance * 1
+  mesh3.position.y = -objectDistance * 2
+
+  mesh1.position.x = 2
+  mesh2.position.x = -2
+  mesh3.position.x = 2
+
+  const sectionMeshes = [mesh1, mesh2, mesh3]
+
+  scene.add(mesh1, mesh2, mesh3);
+
+
+  /**
+   * Lights
+   */
+  const directionalLight = new THREE.DirectionalLight('#666bff', 1)
+  directionalLight.position.set(1, 1, 0)
+  scene.add(directionalLight)
+
+
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+
+  window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+  /**
+   * Camera
+   */
+      // Group
+  const cameraGroup = new THREE.Group()
+  scene.add(cameraGroup)
+
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+  camera.position.z = 6
+  cameraGroup.add(camera)
+
+  /**
+   * Renderer
+   */
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+  })
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+  /**
+   * Scroll
+   */
+  let scrollY = window.scrollY
+  window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+  })
+
+  /**
+   * Cursor
+   */
+  const cursor = {x: 0, y: 0}
+  window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+  })
+
+
+  /**
+   * Animate
+   */
+  const clock = new THREE.Clock()
+  let previousTime = 0;
+
+  const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    // Animate camera
+    camera.position.y = -scrollY / sizes.height * objectDistance
+
+    // Parallax
+    const parallaxX = cursor.x
+    const parallaxY = -cursor.y
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * deltaTime * 5
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * deltaTime * 5
+
+    // Animate meshes
+    sectionMeshes.forEach(mesh => {
+      mesh.rotation.x = elapsedTime * 0.1
+      mesh.rotation.y = elapsedTime * 0.12
+    })
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+  }
+
+  tick()
+});
+</script>
+
+
+<template>
+  <div class="w100">
+    <canvas ref="canvasRef" class="webgl"></canvas>
+
+    <section class="section">
+      <h1>大家好！祝你晚安。</h1>
+    </section>
+    <section class="section">
+      <h2>おはようございます</h2>
+    </section>
+    <section class="section">
+      <h2>@phucbm</h2>
+    </section>
+  </div>
+</template>
+
+<style>
+* {
+  margin:0;
+  padding:0;
+}
+
+html,
+body {
+  overflow:auto;
+  background-color:#1e1a20;
+}
+
+.webgl {
+  position:fixed;
+  top:0;
+  left:0;
+  outline:none;
+}
+
+
+.section {
+  display:flex;
+  align-items:center;
+  height:100vh;
+  position:relative;
+  color:#ffeded;
+  text-transform:uppercase;
+  font-size:7vmin;
+  padding-left:10%;
+  padding-right:10%;
+}
+
+section:nth-child(odd) {
+  justify-content:flex-end;
+}
+</style>
