3d1c3c86e8957eadae96ea8eca039bc0e40aa4f6
phucbui_viivue <phuc.bui@viivue.com>
2023-02-09 21:45:48 +0700

    feat(scroll-based-animation): add particles

--- pages/17.vue
+++ pages/17.vue
@@ -78,6 +78,32 @@ onMounted(() => {
 
 
   /**
+   * Particles
+   */
+// Geometry
+  const particlesCount = 200;
+  const positions = new Float32Array(particlesCount * 3)
+  for(let i = 0; i < particlesCount; i++){
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+    positions[i * 3 + 1] = objectDistance * 0.5 - Math.random() * objectDistance * sectionMeshes.length;
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+  }
+
+  const particlesGeometry = new THREE.BufferGeometry()
+  particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+  // Material
+  const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+  })
+
+  // Points
+  const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+  scene.add(particles)
+
+  /**
    * Lights
    */
   const directionalLight = new THREE.DirectionalLight('#666bff', 1)
