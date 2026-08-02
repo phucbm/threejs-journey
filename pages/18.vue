6092337b0dd3a380166bb8323744c80284e956eb
phucbm <hiphucbui@gmail.com>
2026-08-02 21:26:28 +0700

    Add physics lesson stub

---
+++ pages/18.vue
@@ -0,0 +1,28 @@
+<script setup>
+import * as THREE from 'three';
+
+definePageMeta({title: `Physics - Unfinished`});
+
+const route = useRoute();
+useHead({
+  title: route.meta.title,
+  meta: [
+    //{name: 'description', content: ''},
+    //{property: 'og:description', content: ''},
+    //{property: 'og:image', content: ''},
+  ]
+});
+
+const canvasRef = ref(null);
+
+onMounted(() => {
+  const canvas = canvasRef.value;
+});
+</script>
+
+
+<template>
+  <div>
+    <canvas ref="canvasRef"></canvas>
+  </div>
+</template>
