1d6e0ef6b528c5fe5f1dfaa965c9039e937008ec
phucbm <hiphucbui@gmail.com>
2026-08-03 16:14:07 +0700

    feat: lesson 20 physics > cannonjs > apply force

--- lessons/20-physics/script.js
+++ lessons/20-physics/script.js
@@ -60,10 +60,11 @@ world.defaultContactMaterial = defaultContactMaterial;
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
     mass: 1,
-    position: new CANNON.Vec3(0, 3, 0),
+    position: new CANNON.Vec3(0, 2, 0),
     shape: sphereShape,
     // material: defaultMaterial
 })
+sphereBody.applyLocalForce(new CANNON.Vec3(250, 0, 0), new CANNON.Vec3(0, 0, 0))
 world.addBody(sphereBody)
 
 // floor
@@ -185,6 +186,7 @@ const tick = () => {
     oldElapsedTime = elapsedTime
 
     // Update physics world
+    sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position)
     world.step(1 / 60, 0, 3);
     // console.log(sphereBody.position.y)
 
