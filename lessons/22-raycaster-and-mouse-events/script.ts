06f2b881d3e168554754eb0a748113c8560405a1
phucbm <hiphucbui@gmail.com>
2026-08-05 10:55:16 +0700

    feat: lesson 22 raycaster and mouse events > load model

--- lessons/22-raycaster-and-mouse-events/script.ts
+++ lessons/22-raycaster-and-mouse-events/script.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -37,6 +38,20 @@ object3.position.x = 2
 scene.add(object1, object2, object3)
 
 /**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0).normalize()
+// raycaster.set(rayOrigin, rayDirection)
+//
+// const intersect = raycaster.intersectObject(object2)
+// console.log(intersect)
+//
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects)
+
+/**
  * Sizes
  */
 const sizes = {
@@ -60,6 +75,15 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = -(event.clientY / sizes.height) * 2 + 1
+})
+
+/**
  * Camera
  */
 // Base camera
@@ -80,14 +104,63 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+gltfLoader.load(
+    './models/Duck/glTF-Binary/Duck.glb',
+    (gltf) => {
+        gltf.scene.position.y = -1.2;
+        scene.add(gltf.scene)
+    }
+)
+
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight('#fff', .9);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 2.1);
+directionalLight.position.set(1, .25, 0);
+scene.add(directionalLight);
+
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * .3) * 1.5;
+    object2.position.y = Math.sin(elapsedTime * .6) * 1.5;
+    object3.position.y = Math.sin(elapsedTime * .5) * 1.5;
+
+    // Cast a ray
+    raycaster.setFromCamera(mouse, camera);
+
+    // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0).normalize()
+    // raycaster.set(rayOrigin, rayDirection);
+    //
+    const objectsToTest = [object1, object2, object3];
+    const intersects = raycaster.intersectObjects(objectsToTest);
+
+    for (const object of objectsToTest) {
+        object.material.color.set('#f00');
+    }
+
+    for (const intersect of intersects) {
+        //@ts-ignore
+        intersect.object.material.color.set('#0000ff');
+    }
 
     // Update controls
     controls.update()
